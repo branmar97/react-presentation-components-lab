260e6552a9752614d9983d5364716335ddae01b4
branmar97 <brandonmar97@gmail.com>
2021-02-16 13:19:45 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -4,10 +4,18 @@ import ReactDOM from 'react-dom';
 import SimpleComponent from './components/SimpleComponent';
 import SimplerComponent from './components/SimplerComponent';
 
+changeText = () => {
+
+}
+
 ReactDOM.render(
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent handleClick={() => {
+      return (
+        <div>Hello there</div>
+      )
+    }} />
   </div>,
   document.getElementById('root')
 );
